842300e30b1dac17463567165d55008b41121d44
Ubeyd Gür <ubeydgur41@gmail.com>
2024-02-24 19:18:01 +0300

    ilk

---
+++ src/main/java/com/example/prolab2_1/HelloApplication.java
@@ -0,0 +1,58 @@
+package com.example.prolab2_1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        int windowHeight;
+        int windowWidth;
+        int rectangleSize = 18;
+        int gapSize = 1;
+
+        System.out.println("Enter Window Height: ");
+        Scanner scanner = new Scanner(System.in);
+        windowHeight = scanner.nextInt();
+        System.out.println("Enter Window Width: ");
+        windowWidth = scanner.nextInt();
+
+
+        ArrayList<Rectangle> rectangleArray = new ArrayList<>();
+        for (int y = 0; y < windowHeight; y+=rectangleSize + gapSize){
+            for (int x = 0; x < windowWidth; x+=rectangleSize + gapSize){
+                Rectangle rectangle = new Rectangle(x, y, rectangleSize, rectangleSize);
+                rectangle.setFill(Color.WHITE);
+                rectangleArray.add(rectangle);
+
+            }
+        }
+        Group myGroup = new Group();
+        myGroup.getChildren().addAll(rectangleArray);
+
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(myGroup,windowWidth,windowHeight);
+        scene.setFill(Color.BLACK);
+        stage.setTitle("GOLDEN HUNTER");
+        stage.setScene(scene);
+
+
+
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/prolab2_1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.prolab2_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
