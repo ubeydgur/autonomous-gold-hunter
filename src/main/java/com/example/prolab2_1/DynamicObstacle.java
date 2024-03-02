349f9661431117135187d55e2eed198509f2e538
Ubeyd Gür <ubeydgur41@gmail.com>
2024-03-03 01:37:46 +0300

    refactor DynamicObstacle class

--- src/main/java/com/example/prolab2_1/DynamicObstacle.java
+++ src/main/java/com/example/prolab2_1/DynamicObstacle.java
@@ -1,21 +1,27 @@
 package com.example.prolab2_1;
 
+import javafx.animation.TranslateTransition;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class DynamicObstacle extends ObstacleBase{
+    private String species;
     enum DynamicObstacles {
         Bee,
         Bird
     }
-    DynamicObstacle (String imagePath) throws FileNotFoundException {
-        sizeX = 2;
-        sizeY = 2;
+    DynamicObstacle (String imagePath, String species, int sizeX, int sizeY) throws FileNotFoundException {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.species = species;
         this.imagePath = new FileInputStream(imagePath);
-        image = new Image(imagePath);
+        image = new Image(this.imagePath);
         imageView = new ImageView(image);
+        imageView.setFitHeight(2 * 10);
+        imageView.setFitWidth(2 * 10);
     }
 }
