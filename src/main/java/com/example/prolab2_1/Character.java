5e90b626855cac5afc6f1f9b767274a9558dcc86
Ubeyd Gür <ubeydgur41@gmail.com>
2024-03-04 18:15:02 +0300

    Add properties of Character class

--- src/main/java/com/example/prolab2_1/Character.java
+++ src/main/java/com/example/prolab2_1/Character.java
@@ -1,4 +1,40 @@
 package com.example.prolab2_1;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+enum MotionDirection{
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+}
+
 public class Character {
+    int locationX;
+    int locationY;
+    int characterSizeX;
+    int characterSizeY;
+    InputStream imagePath;
+    ImageView imageView;
+    Image image;
+    MotionDirection direction;
+
+    public Character(String imagePath, int locationX, int locationY, int characterSizeX, int characterSizeY) throws FileNotFoundException {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.characterSizeX = characterSizeX;
+        this.characterSizeY = characterSizeY;
+        this.imagePath = new FileInputStream(imagePath);
+        image = new Image(this.imagePath);
+        imageView = new ImageView(image);
+        imageView.setX(locationX * 10);
+        imageView.setY(locationY * 10);
+        imageView.setFitHeight(characterSizeY * 10);
+        imageView.setFitWidth(characterSizeX * 10);
+    }
 }
